1cf0630c7f086e7ce90bdee2b63ebd6adaabc78f
Popa Daniel <popa.daniel@agilefreaks.com>
2025-10-10 00:40:25 +0300

    Extract functions in separate file

--- nodes/JsonApi/Serializer.node.ts
+++ nodes/JsonApi/Serializer.node.ts
@@ -7,13 +7,13 @@ import {
 	type INodeTypeDescription,
 	NodeOperationError
 } from 'n8n-workflow';
-import { serialize } from './serializer';
+import { serializeFromNode } from './serializer';
 
 export class Serializer implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Json Api Serializer',
 		name: 'jsonApiSerializer',
-		icon: { light: 'file:logo.png', dark: 'file:logo.png' },
+		icon: { light: 'file:logo.svg', dark: 'file:logo.svg' },
 		group: ['transform'],
 		version: 1,
 		description: 'Serialize data to Json Api Specification',
@@ -58,42 +58,31 @@ export class Serializer implements INodeType {
 				description: 'The type of the resource',
 			},
 			{
-				displayName: 'JSON Attributes',
-				name: 'json_resource_attributes',
+				displayName: 'Attributes',
+				name: 'resource_attributes',
 				placeholder: 'Add attributes as json',
 				type: 'json',
 				default: ''
-			},
+			}
 		],
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const response_type = this.getNodeParameter('response_type', 0);
-		const resource_type = this.getNodeParameter('resource_type', 0);
-		const resource_id = this.getNodeParameter('resource_id', 0);
 
-		let json_resource_attributes = this.getNodeParameter('json_resource_attributes', 0) as string;
-		try {
-			json_resource_attributes = JSON.parse(json_resource_attributes);
-		} catch (exception) {
-			throw new NodeOperationError(this.getNode(), 'Attributes must be a valid json');
-		}
-
-		let payload = {}
+		let payload: { data: unknown };
 		let data;
 
 		if (response_type === 'object') {
-			data = serialize(resource_type as string, resource_id as string, json_resource_attributes);
-			payload = { data: data };
-		}
-		else if (response_type === 'array') {
-			data = serialize(resource_type as string, resource_id as string, json_resource_attributes);
-			payload = { data: [data] }
-		}
-		else {
+			data = serializeFromNode(this, 0);
+		} else if (response_type === 'array') {
+			const items = this.getInputData();
+			data = items.map((_, i) => serializeFromNode(this, i));
+		} else {
 			throw new NodeOperationError(this.getNode(), 'Invalid response type');
 		}
 
+		payload = { data };
 		return [this.helpers.returnJsonArray(payload as any)];
 	}
 }
--- nodes/JsonApi/serializer.ts
+++ nodes/JsonApi/serializer.ts
@@ -1,9 +1,14 @@
+import { type IExecuteFunctions, NodeOperationError, type INode } from 'n8n-workflow';
+
 export interface JsonApiResource {
 	id: string;
 	type: string;
 	attributes: unknown;
 }
 
+/**
+ * Serializes a single object to JSON API format
+ */
 export function serialize(
 	resource_type: string,
 	resource_id: string,
@@ -16,3 +21,22 @@ export function serialize(
 	};
 }
 
+export function parseAttributes(node: INode, attributes: string): unknown {
+	try {
+		return JSON.parse(attributes);
+	} catch (exception) {
+		throw new NodeOperationError(node, 'Attributes must be a valid json');
+	}
+}
+
+export function serializeFromNode(
+	context: IExecuteFunctions,
+	itemIndex: number
+): JsonApiResource {
+	const resource_type = context.getNodeParameter('resource_type', itemIndex) as string;
+	const resource_id = context.getNodeParameter('resource_id', itemIndex) as string;
+	const resource_attributes = context.getNodeParameter('resource_attributes', itemIndex) as string;
+
+	const attributes = parseAttributes(context.getNode(), resource_attributes);
+	return serialize(resource_type, resource_id, attributes);
+}
